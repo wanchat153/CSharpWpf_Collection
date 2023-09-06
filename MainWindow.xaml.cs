737bea797159276e1d8a6ab41f97229150a1ae74
M153 <wanchatgame@gmail.com>
2023-09-06 21:24:36 +0700

    ArrayList

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,48 @@ namespace CSharpWpf_Collection
     /// </summary>
     public partial class MainWindow : Window
     {
+        ArrayList arrayList;
         public MainWindow()
         {
             InitializeComponent();
+            arrayList = new ArrayList();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void removeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void showBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Add data
+            arrayList.Add(10);
+            arrayList.Add(15);
+            arrayList.Add(16);
+            arrayList.Add(10);
+            arrayList.Add(18);
+
+            //remove (10)Object
+            arrayList.Remove(10);
+            //remove (2)index
+            arrayList.RemoveAt(2);
+            //show low to high
+            arrayList.Sort();
+
+            foreach (var item in arrayList)
+            {
+                //Show All
+                MessageBox.Show(item.ToString());
+            }
+
+            //Count data
+            MessageBox.Show("Count: "+ arrayList.Count.ToString());
+
         }
     }
 }
