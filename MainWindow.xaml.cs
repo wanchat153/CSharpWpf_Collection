029cff73d996abfba7a3561c638f3ab1ec52cb71
M153 <wanchatgame@gmail.com>
2023-09-06 22:14:56 +0700

    Update
    add remove showall

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,29 +30,18 @@ namespace CSharpWpf_Collection
 
         private void addBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            //Add data
+            arrayList.Add(textName.Text);
         }
 
         private void removeBtn_Click(object sender, RoutedEventArgs e)
         {
-            
+            //remove ()Object
+            arrayList.Remove(textName.Text);
         }
 
         private void showBtn_Click(object sender, RoutedEventArgs e)
         {
-            //Add data
-            arrayList.Add(10);
-            arrayList.Add(15);
-            arrayList.Add(16);
-            arrayList.Add(10);
-            arrayList.Add(18);
-
-            //remove (10)Object
-            arrayList.Remove(10);
-            //remove (2)index
-            arrayList.RemoveAt(2);
-            //show low to high
-            arrayList.Sort();
 
             foreach (var item in arrayList)
             {
